0abd0e87292a343f15da66f21509042edb5c7226
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-04-20 22:24:24 +0700

    add card component

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// const Card = (props) => {
+    const Card = ({name, email, id}) => {
+
+    //using destructuring
+    // const  {name, email, id} = props;
+    return (
+        <div className='tc bg-light-green dib br3 pd3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src= {`https://robohash.org/${id}?200x20`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+} 
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Segy',
+        username: 'segy',
+        email: 'segy@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Hendro',
+        username: 'hendro',
+        email: 'hendro@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Pratama',
+        username: 'pratama',
+        email: 'pratama@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Sherora',
+        username: 'sherora',
+        email: 'sherora@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'SegyHP',
+        username: 'segyhp',
+        email: 'segyhp@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Segy Hendro Pratama',
+        username: 'segyhendropratama',
+        email: 'segyhendropratama@gmail.com'
+    },
+];
