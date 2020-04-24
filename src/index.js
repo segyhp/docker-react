2101f0835bca676296b3a1c442e9674adc2cd604
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-04-24 21:28:37 +0700

    feat: add search function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import CardList from './CardList';
 import 'tachyons';
-import { robots } from './robots';
+import App from './App';
 
-ReactDOM.render(<CardList robots={robots} />,
+ReactDOM.render(<App />,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
