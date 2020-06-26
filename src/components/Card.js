fdb4905d7fe690f12fc3ea978846a02fac9c0c5c
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-06-26 20:16:38 +0700

    feat: pwa optimization

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ import React from 'react';
     return (
         <div className='tc bg-light-green dib br3 pd3 ma2 grow bw2 shadow-5'>
             <h1>Robot Card</h1>
-            <img alt='robots' src= {`https://robohash.org/${id}?200x20`}/>
+            <img alt='robots' src= {`https://robohash.org/${id}?200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,6 +5,7 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className='pa2'>
             <input
+            aria-label="Search Robots"
             type="search"
             placeholder="Search robots"
             className="pa3 ba b--green bg-lightest-blue"
