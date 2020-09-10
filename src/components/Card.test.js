59e082aa88ef558d8df3d2ca39ed32ab0bb77308
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-09-10 22:25:11 +0700

    tests: add snapshot test for component

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,14 @@
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Card from './Card';
+import toJson, {createSerializer} from 'enzyme-to-json';
+
 
 it('expect to render Card Component', () => {
-    expect(shallow(<Card />).length).toEqual(1);
+    //what shallow do is create simple component
+    //what mount do is create full component and need dom API to work or browser /headless browser
+    //what render do is between shallow and mount, just for render but not fully functional
+    // expect(shallow(<Card />).length).toEqual(1);
+    //create snapshot
+    expect(toJson(shallow(<Card />))).toMatchSnapshot();
 });
